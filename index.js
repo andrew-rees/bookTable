cbf16b682bf39607183740fde243fcf07b26d109
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-06-01 17:07:50 +0100

    stuck on for loop

--- index.js
+++ index.js
@@ -26,47 +26,64 @@ const readline = require('readline-sync')
 //PROMGRAMME STARTS//
 //////////////////////
 
-console.log(`Choose your column width (recommend between 30 and 50:`)
-const userWidthInput = readline.prompt();
-const width = +userWidthInput
 
 
-//function to set length of string, replace excess letter with ... and fill deficient strings with spaces
-let titleString = "Lord of the Rings";
-let titleStringWidth = width
-//console.log(titleString.length);
-function setTitleString (){
-    if (titleString.length > titleStringWidth){
-        return `${titleString.substring(0,(titleStringWidth-3))}...`;
-    } else if (titleString.length < titleStringWidth){
-        return `${" ".repeat(titleStringWidth - titleString.length)} ${titleString.substring(0, titleStringWidth)}`;
-    } else {
-        return `${titleString.substring(0, titleStringWidth)}`
+//class of book
+// class Book {
+//     constructor (ref, published, title, author) {
+//         this.ref = ref
+//         this.published = published
+//         this.title = title
+//         this.author = author
+//     }
+// }
+// var lotr = new Book ("lotr", "29/07/1954", "Lord of the Rings", "John Ronald Reuel Tolkien")
+// var got = new Book ("got", "01/08/1996", "A Game of Thrones", "George Raymond Martin")
+// var hp = new Book ("hp", "21/06/2003", "Harry Potter and the Order of the Phoenix", "Joanne Rowling")
+
+// arrays of titles and authors
+publishedArray = [];
+titleArray = ["Lord of the Rings", "A Game of Thrones", "Harry Potter and the Order of the Phoenix"];
+authorArray = ["John Ronald Reuel Tolkien", "George Raymond Martin", "Joanne Rowling"];
+
+// console.log("titleArray0 = " + titleArray[0] + " and authorArray0 = " + authorArray[0]);
+// console.log("titleArray1 = " + titleArray[1] + " and authorArray1 = " + authorArray[1]);
+// console.log("titleArray2 = " + titleArray[2] + " and authorArray2 = " + authorArray[2]);
+
+
+if (i = 0, i > 3, i++) {
+
+    //function to set Title column width, add spaces to start or ... to end if string.length <> width
+    let titleString = titleArray[i];
+    //console.log(titleString.length);
+    function setTitleString() {
+        if (titleString.length > 30) {
+            return `${titleString.substring(0,(30-3))}...`;
+        } else if (titleString.length < 30) {
+            return `${" ".repeat(30 - titleString.length)} ${titleString.substring(0, 30)}`;
+        } else {
+            return `${titleString.substring(0, 30)}`
+        }
+
     }
-    
-}
-//console.log(setTitleString())
+    //console.log(setTitleString())
 
+    //function to set Author column width, add spaces to start or ... to end if string.length <> width
+    let authorString = authorArray[i];
+    //console.log(authorString.length);
+    function setAuthorString() {
+        if (authorString.length > 20) {
+            return `${authorString.substring(0,(20 - 3))}...`;
+        } else if (authorString.length < 20) {
+            return `${" ".repeat(20 - authorString.length)} ${authorString.substring(0, 20)}`;
+        } else {
+            return `${authorString.substring(0, 20)}`
+        }
 
-let authorString = "John Ronald Reuel Tolkien";
-let authorStringWidth = width - 10
-//console.log(authorString.length);
-function setAuthorString (){
-    if (authorString.length > authorStringWidth){
-        return `${authorString.substring(0,(authorStringWidth - 3))}...`;
-    } else if (authorString.length < authorStringWidth){
-        return `${" ".repeat(authorStringWidth - authorString.length)} ${authorString.substring(0, authorStringWidth)}`;
-    } else {
-        return `${authorString.substring(0, authorStringWidth)}`
     }
-    
+    //console.log(setAuthorString())
+
+    //output
+    console.log(`| ${setTitleString()} | ${setAuthorString()} |`)
+
 }
-//console.log(setAuthorString())
-
-
-
-
-
-
-
-console.log(`| ${setTitleString()} | ${setAuthorString()} |`)
