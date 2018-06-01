dbe3d0d995cc781972a8df0ab1ff0155dd46c530
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-06-01 17:48:06 +0100

    last push

--- index.js
+++ index.js
@@ -1,62 +1,22 @@
 const readline = require('readline-sync')
 
-// let number = 2;
-
-// console.log("You have " + number + " brain cells"); // 2 strings
-
-// console.log(`You have ${number} brain cells`); // 1 string
-
-
-
-
-
-
-//get the text
-
-"Publication Date,Title,Authors"
-"\n29/07/1954,Lord of the Rings,John Ronald Reuel Tolkien"
-"\n01/08/1996,A Game of Thrones,George Raymond Martin"
-"\n21/06/2003,Harry Potter and the Order of the Phoenix,Joanne Rowling"
-
-//each line will be `${date formatting} | text with ${truncation rule} | text with ${truncation rule} |`
-//truncation rule = 
-
-
 ////////////////////////
 //PROMGRAMME STARTS//
 //////////////////////
 
-
-
-//class of book
-// class Book {
-//     constructor (ref, published, title, author) {
-//         this.ref = ref
-//         this.published = published
-//         this.title = title
-//         this.author = author
-//     }
-// }
-// var lotr = new Book ("lotr", "29/07/1954", "Lord of the Rings", "John Ronald Reuel Tolkien")
-// var got = new Book ("got", "01/08/1996", "A Game of Thrones", "George Raymond Martin")
-// var hp = new Book ("hp", "21/06/2003", "Harry Potter and the Order of the Phoenix", "Joanne Rowling")
-
 // arrays of titles and authors
-publishedArray = [];
+publishedArray = ["29/07/1954", "01/08/1996", "21/06/2003"];
 titleArray = ["Lord of the Rings", "A Game of Thrones", "Harry Potter and the Order of the Phoenix"];
 authorArray = ["John Ronald Reuel Tolkien", "George Raymond Martin", "Joanne Rowling"];
 
-// console.log("titleArray0 = " + titleArray[0] + " and authorArray0 = " + authorArray[0]);
-// console.log("titleArray1 = " + titleArray[1] + " and authorArray1 = " + authorArray[1]);
-// console.log("titleArray2 = " + titleArray[2] + " and authorArray2 = " + authorArray[2]);
 
 //function to set Title column width, add spaces to start or ... to end if string.length <> width
 let titleString = ""
 function setTitleString() {
         if (titleString.length > 30) {
-            return `${titleString.substring(0,(30-3))}...`;
+            return `${titleString.substring(0,(30 - 3))}...`;
         } else if (titleString.length < 30) {
-            return `${" ".repeat(30 - titleString.length)} ${titleString.substring(0, 30)}`;
+            return `${" ".repeat(30 - (titleString.length + 1))} ${titleString.substring(0, 30)}`;
         } else {
             return `${titleString.substring(0, 30)}`
         }
@@ -75,11 +35,35 @@ function setAuthorString() {
         }
 
     }
+///////////
+//output//
+//////////
 
-for (i = 0; i > 3; i++) {
+console.log(`| Pub Date   |                           Title |              Authors |`)
+
+for (i = 0; i < 3; i++) {
     titleString = titleArray[i];
     authorString = authorArray[i];
     //output
-    console.log(`| ${setTitleString()} | ${setAuthorString()} |`)
+    console.log(`| ${publishedArray[i]} | ${setTitleString()} | ${setAuthorString()} |`)
 
 }
+
+
+
+
+
+
+
+//class of book
+// class Book {
+//     constructor (ref, published, title, author) {
+//         this.ref = ref
+//         this.published = published
+//         this.title = title
+//         this.author = author
+//     }
+// }
+// var lotr = new Book ("lotr", "29/07/1954", "Lord of the Rings", "John Ronald Reuel Tolkien")
+// var got = new Book ("got", "01/08/1996", "A Game of Thrones", "George Raymond Martin")
+// var hp = new Book ("hp", "21/06/2003", "Harry Potter and the Order of the Phoenix", "Joanne Rowling")
